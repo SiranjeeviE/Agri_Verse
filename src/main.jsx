fba6dc85e9d3754bc05873ea9d2e09038b3fc623
SiranjeeviE <SiranjeeviE@users.noreply.github.com>
2026-01-21 10:50:10 +0530

    Add src\main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./i18n/config";
+createRoot(document.getElementById("root")).render(<App />);
